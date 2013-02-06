f605c640bb3e98654b883af458365729c2734694
MDSilber <mds2161@columbia.edu>
2013-02-05 20:03:35 -0500

    Added lockbox class, and the circuit diagram

---
+++ iOS/LockBox/LockBox/LockBox.h
@@ -0,0 +1,19 @@
+//
+//  LockBox.h
+//  LockBox
+//
+//  Created by Mason Silber on 2/5/13.
+//  Copyright (c) 2013 Mason Silber. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface LockBox : NSManagedObject
+
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSString * ipAddress;
+@property (nonatomic, retain) NSNumber * isLocked;
+
+@end
---
+++ iOS/LockBox/LockBox/LockBox.m
@@ -0,0 +1,28 @@
+//
+//  LockBox.m
+//  LockBox
+//
+//  Created by Mason Silber on 2/5/13.
+//  Copyright (c) 2013 Mason Silber. All rights reserved.
+//
+
+#import "LockBox.h"
+
+
+@implementation LockBox
+
+@dynamic name;
+@dynamic ipAddress;
+@dynamic isLocked;
+
+-(id)init
+{
+    self = [super init];
+    if(self)
+    {
+        //Note @1 is "object" version of the integer 1
+        [self setIsLocked:@1];
+    }
+    return self;
+}
+@end
