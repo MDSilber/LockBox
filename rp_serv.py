3b0e53553b5580da8846c403b7e49c86fb9fed9b
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2013-04-07 17:06:47 -0400

    processes get parameters now

--- rp_serv.py
+++ rp_serv.py
@@ -11,11 +11,17 @@ class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         self.end_headers()
         
         response['response'] = 200
+        response['path'] = self.path
+        response['params'] = self.process_params(self.path)
         self.wfile.write(json.dumps(response))
 
         self.wfile.close()
 
-
+    def process_params(self,path):
+        path = "".join(path.split("?")[1:])
+        params = dict(p.split('=', 1) for p in path.split('&'))
+        print params
+        return params
         
 
 
@@ -24,6 +30,6 @@ PORT = 4567
 Handler = MyHandler
 httpd = BaseHTTPServer.HTTPServer((HOST, PORT), Handler)
 httpd.socket = ssl.wrap_socket (httpd.socket, certfile='server.pem', server_side=True)
-print 'serving on ', HOST, ":", PORT
+print 'serving on ' + HOST + ":" + PORT
 httpd.serve_forever()
 
